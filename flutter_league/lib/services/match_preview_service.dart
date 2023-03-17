f8ca1d66c1707f9cef7b65cd6fc817370f9c2636
Csuka Balázs <csukabalazs1219@gmail.com>
2023-03-17 23:49:20 +0100

    little service refact

--- flutter_league/lib/services/match_preview_service.dart
+++ flutter_league/lib/services/match_preview_service.dart
@@ -3,42 +3,50 @@ import 'package:flutter_riot_api/model/match_preview.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_riot_api/utils/constants.dart';
 
+// Returns a MatchPreview object containing information about the specified match
 Future<MatchPreview?> getMatchPreview(
     String summonerName, String matchId) async {
   final matchResponse = await http.get(
-    //TODO server aztonosito külön
-    Uri.parse('${apiUrl2}match/v5/matches/$matchId?api_key=$apikey'),
+    Uri.parse(
+        '${apiUrl2}match/v5/matches/$matchId?api_key=$apikey'), // Send a GET request to the Riot API to fetch match data
   );
 
   if (matchResponse.statusCode == 200) {
+    // If the request was successful
     final matchData = jsonDecode(matchResponse.body);
+
+    // Find the participant object for the specified summoner
     Map<String, dynamic> participant = matchData["info"]["participants"]
         .firstWhere((p) => p["summonerName"] == summonerName,
             orElse: () => null);
 
+    // Parse the match data into a MatchPreview object
     final summonerObjResponse = MatchPreview.fromJson(
         matchData["info"], participant, matchData["metadata"]["matchId"]);
-    return summonerObjResponse;
+    return summonerObjResponse; // Return the MatchPreview object
   } else {
     // TODO: handle error
     return null;
   }
 }
 
-Future<List<String>> getMatchIds(
-    //https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/3nAQ1XUk_evbC8AqTcH06blBip0dzrpFQVl-PRGP962RTdzfWkWWfApcwOy09C1U1R59kl-LoVI8rA/ids?start=0&count=100&api_key=RGAPI-90c0dff8-1e1b-43b7-b313-0d43d6bf19b1
-    String puuid) async {
+// Returns a list of match IDs for a given summoner PUUID
+Future<List<String>> getMatchIds(String puuid) async {
   List<String> response = [];
+
   final matchIdResponse = await http.get(
-    //TODO server aztonosito külön
     Uri.parse(
-        '${apiUrl2}match/v5/matches/by-puuid/$puuid/ids?start=0&count=100&api_key=$apikey'),
+        '${apiUrl2}match/v5/matches/by-puuid/$puuid/ids?start=0&count=100&api_key=$apikey'), // Send a GET request to the Riot API to fetch match IDs for the summoner
   );
+
   if (matchIdResponse.statusCode == 200) {
-    response = jsonDecode(matchIdResponse.body).cast<String>();
-    return response;
+    // If the request was successful
+    response = jsonDecode(matchIdResponse.body)
+        .cast<String>(); // Parse the response into a list of strings
+    return response; // Return the list of match IDs
   } else {
     // TODO: handle error
   }
+
   return response;
 }
--- flutter_league/lib/services/summoner_service.dart
+++ flutter_league/lib/services/summoner_service.dart
@@ -3,6 +3,10 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_riot_api/model/summoner.dart';
 import 'package:flutter_riot_api/utils/constants.dart';
 
+// This function retrieves the Summoner object associated with the given Summoner Name.
+// If a Summoner is found, it also retrieves the Summoner's ranked data, and merges
+// the Summoner and Ranked data into a single JSON object. If a Summoner is not found,
+// null is returned.
 Future<Summoner?> getSummonerByName(String summonerName) async {
   final summonerResponse = await http.get(
     Uri.parse(
@@ -11,10 +15,16 @@ Future<Summoner?> getSummonerByName(String summonerName) async {
 
   if (summonerResponse.statusCode == 200) {
     final summonerData = jsonDecode(summonerResponse.body);
+
+    // Retrieve the Summoner's ranked data.
     final rankedData = await getRankedData(summonerData['id']);
+
+    // If the Summoner has ranked data, merge the Summoner and Ranked data into a single JSON object.
     final jsonResult = (rankedData[0]['result'] != 'Unranked')
         ? mergeJson(summonerData, rankedData)
         : summonerData;
+
+    // Create and return the Summoner object.
     final summonerObjResponse = Summoner.fromJson(jsonResult);
     return summonerObjResponse;
   } else {
@@ -23,6 +33,9 @@ Future<Summoner?> getSummonerByName(String summonerName) async {
   }
 }
 
+// This function retrieves the ranked data associated with the given Account ID.
+// If ranked data is found, it is returned as a List<dynamic>. If ranked data is not
+// found, a single JSON object with a 'result' key of 'Unranked' is returned.
 Future<List<dynamic>> getRankedData(String accountId) async {
   final rankedResponse = await http.get(
     Uri.parse(
@@ -31,21 +44,27 @@ Future<List<dynamic>> getRankedData(String accountId) async {
 
   if (rankedResponse.statusCode == 200) {
     final rankedData = jsonDecode(rankedResponse.body);
+
+    // If ranked data is found, return it as a List<dynamic>.
     if (rankedData.isNotEmpty) {
       return rankedData;
     }
   }
 
+  // If ranked data is not found, return a single JSON object with a 'result' key of 'Unranked'.
   return [
     {'result': 'Unranked'}
   ];
 }
 
+// This function merges two JSON objects into a single JSON object.
+// It is used to merge the Summoner and Ranked data into a single JSON object.
 Map<String, dynamic> mergeJson(
     Map<String, dynamic> summonerData, List<dynamic> rankedData) {
   Map<String, dynamic> rankedInfo = {};
   if (rankedData.isNotEmpty) {
     rankedData.forEach((data) {
+      // Extract the ranked data from the list and add it to the rankedInfo Map.
       rankedInfo[data['queueType']] = {
         'tier': data['tier'],
         'rank': data['rank'],
@@ -56,6 +75,7 @@ Map<String, dynamic> mergeJson(
     });
   }
 
+  // Merge the Summoner and Ranked data into a single JSON object.
   Map<String, dynamic> mergedData = {...summonerData, ...rankedInfo};
   return mergedData;
 }
